5bdbfdd81808b587cac3dc9e51b2749f887f65c7
shicky1223 <shatkratuswarnkar22@gmail.com>
2023-10-04 15:47:36 -0400

    added all necessary components for this project. working smoothly

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function calculateResult() {
+    const expression = document.getElementById("display").value;
+    try {
+        const result = eval(expression);
+        document.getElementById("display").value = result;
+    } catch (error) {
+        document.getElementById("display").value = "Error";
+    }
+}
 function add(array){
     if (array.length === 0) return 0;
     let sum = 0;
